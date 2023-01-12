a919d947e9f5196e71f594a464e5158ae7fa2014
minyeong25 <ownpace802@gmail.com>
2023-01-12 18:04:14 +0900

    first commit

---
+++ jigu_shop/js/sub2page.js
@@ -0,0 +1,11 @@
+$(function () {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: true,
+    items: 1,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+  });
+});

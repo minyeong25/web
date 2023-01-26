2714b4d07d7505e4c5abd694b7641fc1d613b53a
minyeong25 <ownpace802@gmail.com>
2023-01-26 18:00:05 +0900

    commit

--- gentlemonster/js/index2.js
+++ gentlemonster/js/index2.js
@@ -96,8 +96,6 @@ $(function () {
     draggable: true,
   });
 
-  // contents2 style 가로스크롤 이벤트
-
   $(window).on("mousewheel scroll", function () {
     let wd = $(this).scrollTop();
     console.log(wd);
@@ -108,50 +106,63 @@ $(function () {
     } else {
       $("header").removeClass("fixed");
     }
-    let elm = $(".box");
-
-    $(elm).each(function (index) {
-      $(this).on("mousewheel", function (e) {
-        let delta = 0;
-        if (event.wheelDelta) {
-          delta = event.wheelDelta / 120;
-        }
-        let moveleft = $(window).scrollLeft();
-        let elmSelecter = $(elm).eq(index);
-
-        if (delta < 0) {
-          /*if (wd >= 1700 && wd <= 2500) */
-          if (wd >= 1000 && wd <= 1600) {
-            $("#contents2").addClass("on");
-          } //else {
-          //   $("#contents2").removeClass("on");
-          // }
-          if ($(elmSelecter).next() != undefined) {
-            try {
-              moveleft = $(elmSelecter).next().offset().left;
-            } catch (e) {
-              $("#contents2").removeClass("on");
-              moveleft = 0;
-            }
-          }
-          // 마우스휠을 아래에서 위로
-        } else {
-          if (wd <= 1700 && wd >= 900) {
-            $("#contents2").addClass("on");
-          } // else {
-          //   $("#contents2").removeClass("on");
-          // }
-          if ($(elmSelecter).prev() != undefined) {
-            try {
-              moveleft = $(elmSelecter).prev().offset().left;
-            } catch (e) {
-              $("#contents2").removeClass("on");
-            }
-          }
-        }
-        // 화면 이동 0.8초(800)
-        $("html,body").stop().animate({ scrollLeft: moveleft }, 800);
-      });
-    });
   });
+  // contents2 style 가로스크롤 이벤트
+
+  // $(window).on("mousewheel scroll", function () {
+  //   let wd = $(this).scrollTop();
+  //   console.log(wd);
+
+  //   let len = $(".box").length;
+  //   if (wd >= 80) {
+  //     $("header").addClass("fixed");
+  //   } else {
+  //     $("header").removeClass("fixed");
+  //   }
+  //   let elm = $(".box");
+
+  //   $(elm).each(function (index) {
+  //     $(this).on("mousewheel", function (e) {
+  //       let delta = 0;
+  //       if (event.wheelDelta) {
+  //         delta = event.wheelDelta / 120;
+  //       }
+  //       let moveleft = $(window).scrollLeft();
+  //       let elmSelecter = $(elm).eq(index);
+
+  //       if (delta < 0) {
+  //         /*if (wd >= 1700 && wd <= 2500) */
+  //         if (wd >= 1000 && wd <= 1600) {
+  //           $("#contents2").addClass("on");
+  //         } //else {
+  //         //   $("#contents2").removeClass("on");
+  //         // }
+  //         if ($(elmSelecter).next() != undefined) {
+  //           try {
+  //             moveleft = $(elmSelecter).next().offset().left;
+  //           } catch (e) {
+  //             $("#contents2").removeClass("on");
+  //             moveleft = 0;
+  //           }
+  //         }
+  //         // 마우스휠을 아래에서 위로
+  //       } else {
+  //         if (wd <= 1700 && wd >= 900) {
+  //           $("#contents2").addClass("on");
+  //         } // else {
+  //         //   $("#contents2").removeClass("on");
+  //         // }
+  //         if ($(elmSelecter).prev() != undefined) {
+  //           try {
+  //             moveleft = $(elmSelecter).prev().offset().left;
+  //           } catch (e) {
+  //             $("#contents2").removeClass("on");
+  //           }
+  //         }
+  //       }
+  //       // 화면 이동 0.8초(800)
+  //       $("html,body").stop().animate({ scrollLeft: moveleft }, 800);
+  //     });
+  //   });
+  // });
 });

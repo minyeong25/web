7c46240a75fadfd4e7ccbf8990008d1553fa5833
minyeong25 <ownpace802@gmail.com>
2023-01-27 13:09:09 +0900

    commit

--- portfolio/js/index.js
+++ portfolio/js/index.js
@@ -1,1 +1,13 @@
-$(function () {});
+$(function () {
+  let nav = $("#nav").offset().top;
+
+  $(window).scroll(function () {
+    let window = $(this).scrollTop();
+    if (nav <= window) {
+      $("#nav").addClass("fixed");
+    } else {
+      $("#nav").removeClass("fixed");
+    }
+    console.log(window);
+  });
+});
